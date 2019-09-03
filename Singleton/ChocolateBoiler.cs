379659c66e57e47d0865fcf3d4607d456e495b5d
helinton.fioramonte <helinton.fioramonte@iatec.com>
2019-09-03 09:13:15 -0300

    Finalizado o padrão singleton

---
+++ Singleton/ChocolateBoiler.cs
@@ -0,0 +1,49 @@
+﻿namespace Singleton
+{
+    internal class ChocolateBoiler
+    {
+        private static ChocolateBoiler Instance { get; set; }
+        private bool Empty { get; set; }
+        private bool Boiled { get; set; }
+
+        private ChocolateBoiler()
+        {
+            Empty = true;
+            Boiled = false;
+        }
+
+        public static ChocolateBoiler GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new ChocolateBoiler();
+            }
+            return Instance;
+        }
+
+        public void Fill()
+        {
+            if (Empty)
+            {
+                Empty = false;
+                Boiled = false;
+            }
+        }
+
+        public void Drain()
+        {
+            if (!Empty && Boiled)
+            {
+                Empty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!Empty && !Boiled)
+            {
+                Boiled = true;
+            }
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var factory = ChocolateBoiler.GetInstance();
+            var factory2 = ChocolateBoiler.GetInstance();
+
+            factory.Boil();
+
+            factory.Drain();
+
+            factory.Fill();
+        }
+    }
+}
